c0f4ffce6ee5e5de51b60c566b3ff7215f92d98a
Kuaruou <50903182+Kuaruou@users.noreply.github.com>
2019-09-27 13:18:19 +0800

    Update all.js

--- js/all.js
+++ js/all.js
@@ -1,5 +1,5 @@
 // 串接API
-var url = 'https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97';
+var url = 'data/datastore_search.json';
 
 var initialData = $.ajax({
     type: "GET",
